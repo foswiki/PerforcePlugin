6c3a00620b3de0be464a0e9458ac7cd155e31fae
OlivierRaginel <OlivierRaginel@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2012-05-04 13:27:49 +0000

    Item11808: perltidy them all, so people are not accidentally impacted by the new enforced rule -- Sorry Micha, could not find a way to define specific values within the file
    git-svn-id: http://svn.foswiki.org/trunk/PerforcePlugin@14686 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- test/unit/PerforcePlugin/PerforcePluginSuite.pm
+++ test/unit/PerforcePlugin/PerforcePluginSuite.pm
@@ -3,8 +3,8 @@ package EmptyPluginSuite;
 use Unit::TestSuite;
 our @ISA = qw( Unit::TestSuite );
 
-sub name { 'EmptyPluginSuite' };
+sub name { 'EmptyPluginSuite' }
 
-sub include_tests { qw(EmptyPluginTests) };
+sub include_tests { qw(EmptyPluginTests) }
 
 1;
